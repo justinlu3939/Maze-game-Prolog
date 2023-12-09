f45997df68286096bfe9137c1f8fb164e8beef04
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2023-12-09 17:52:16 -0600

    Update project2.pl
    with an unbound second parameter

--- project2.pl
+++ project2.pl
@@ -1,46 +1,45 @@
+% Predicate to initiate the search for an exit
 find_exit(Maze, Actions) :-
     start_position(Maze, Start),
     end_position(Maze, End),
     write('starting coords: '), write(Start), nl, write('end coords: '), write(End), nl,
-    % now Start and End will hold the coordinates of the starting and ending coordinates
-    valid_actions(Actions),
-    traverse_maze(Start, Maze, Actions),
+    depth_first_search(Start, End, Maze, [Start], [], Actions),
     write('Sucess. the path is '), write(Actions).
+
+% Depth-first search algorithm
+depth_first_search(End, End, _, _, Actions, FinalActions) :-
+    reverse(Actions, FinalActions).
+depth_first_search(Position, End, Maze, Visited, Acc, Actions) :-
+    move(Position, Maze, Move, NewPosition),
+    \+ member(NewPosition, Visited), % Avoid cycles
+    depth_first_search(NewPosition, End, Maze, [NewPosition|Visited], [Move|Acc], Actions).
+
+% Define movements based on actions and defines the NewPosition
+move((X, Y), Maze, left, (X1, Y)) :- X1 is X - 1, valid_position((X1, Y), Maze).
+move((X, Y), Maze, right, (X1, Y)) :- X1 is X + 1, valid_position((X1, Y), Maze).
+move((X, Y), Maze, up, (X, Y1)) :- Y1 is Y - 1, valid_position((X, Y1), Maze).
+move((X, Y), Maze, down, (X, Y1)) :- Y1 is Y + 1, valid_position((X, Y1), Maze).
+
+% Check if the new position is valid (not a wall or outside the maze)
+valid_position((X, Y), Maze) :-
+    nth0(Y, Maze, Row),
+    nth0(X, Row, Cell),
+    Cell \= w,
+    Cell \= '|',
+    Cell \= '--',
+    \+ (X < 0; Y < 0),
+    length(Maze, NumRows),
+    length(Row, NumCols),
+    X < NumCols,
+    Y < NumRows.
+
 
 % Find the start position within the maze
 start_position(Maze, (X, Y)) :-
     nth0(Y, Maze, Row),
     nth0(X, Row, s).
 
-% find the end position within the maze
+% Find the end position within the maze
 end_position(Maze, (X, Y)) :-
     nth0(Y, Maze, Row),
     nth0(X, Row, e).
-
-% check the validity of the moves made by the user
-% Check if the list of actions is valid
-valid_actions([]).
-valid_actions([Action|Rest]) :-
-    member(Action, [left, right, up, down]),
-    valid_actions(Rest).
-
-% Traverse the maze using the list of actions
-traverse_maze(Position, Maze, []) :-
-    end_position(Maze, Position).
-traverse_maze(Position, Maze, [Action|Rest]) :-
-    move(Action, Position, NewPosition),
-    valid_position(NewPosition, Maze),
-    % (end_position(Maze, NewPosition) -> true);
-    traverse_maze(NewPosition, Maze, Rest).
-
-% Define movements based on actions and defines the NewPosition
-move(left, (X, Y), (X1, Y)) :- X1 is X - 1.
-move(right, (X, Y), (X1, Y)) :- X1 is X + 1.
-move(up, (X, Y), (X, Y1)) :- Y1 is Y - 1.
-move(down, (X, Y), (X, Y1)) :- Y1 is Y + 1.
-
-% Check if the new position is valid (not a wall or outside the maze)
-valid_position((X, Y), Maze) :-
-    nth0(Y, Maze, Row),
-    nth0(X, Row, Cell),
-    \+ member(Cell, [w]). % 'w' represents a wall
