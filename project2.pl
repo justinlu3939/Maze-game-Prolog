737e6b4e6f3706f3470329944cc3bab1750de19b
justinlu3939 <98128770+justinlu3939@users.noreply.github.com>
2023-12-09 17:15:48 -0600

    Update project2.pl

--- project2.pl
+++ project2.pl
@@ -1,17 +1,11 @@
-% find_exit(Maze) :-
-% this predicate is found in the find_exit.pl, solved by adding find_value/6 in the module of test.pl
-% find the coordinate of the starting position:: total success, it returns the starting coordinate
-% find_value(Maze, s, 0, 0, [], Start),
-% find_value(Maze, e, 0, 0, [], End),
-% write('starting coords: '), write(Start), nl, write('end coords: '), write(End).
-% Summary of this code: it works well, just the code at the bottom gets the start and end coordinates better
-
 find_exit(Maze, Actions) :-
     start_position(Maze, Start),
     end_position(Maze, End),
-    write('starting coords: '), write(Start), nl, write('end coords: '), write(End),
+    write('starting coords: '), write(Start), nl, write('end coords: '), write(End), nl,
     % now Start and End will hold the coordinates of the starting and ending coordinates
-    valid_actions(Actions).
+    valid_actions(Actions),
+    traverse_maze(Start, Maze, Actions),
+    write('Sucess. the path is '), write(Actions).
 
 % Find the start position within the maze
 start_position(Maze, (X, Y)) :-
@@ -29,3 +23,24 @@ valid_actions([]).
 valid_actions([Action|Rest]) :-
     member(Action, [left, right, up, down]),
     valid_actions(Rest).
+
+% Traverse the maze using the list of actions
+traverse_maze(Position, Maze, []) :-
+    end_position(Maze, Position).
+traverse_maze(Position, Maze, [Action|Rest]) :-
+    move(Action, Position, NewPosition),
+    valid_position(NewPosition, Maze),
+    % (end_position(Maze, NewPosition) -> true);
+    traverse_maze(NewPosition, Maze, Rest).
+
+% Define movements based on actions and defines the NewPosition
+move(left, (X, Y), (X1, Y)) :- X1 is X - 1.
+move(right, (X, Y), (X1, Y)) :- X1 is X + 1.
+move(up, (X, Y), (X, Y1)) :- Y1 is Y - 1.
+move(down, (X, Y), (X, Y1)) :- Y1 is Y + 1.
+
+% Check if the new position is valid (not a wall or outside the maze)
+valid_position((X, Y), Maze) :-
+    nth0(Y, Maze, Row),
+    nth0(X, Row, Cell),
+    \+ member(Cell, [w]). % 'w' represents a wall
